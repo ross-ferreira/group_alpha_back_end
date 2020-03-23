7392f744f2bb68b0f47294c4d96baf563a9f47bd
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-03-23 16:48:10 +0000

    created images table and migrated table

---
+++ database/migrations/2020_03_23_160852_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string("url",100);
+            $table->string("title",100);
+            $table->integer("top");
+            $table->integer("left");
+            $table->integer("bottom");
+            $table->integer("right");
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
